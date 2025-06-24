860e959f1198521514aa77839e071fbd0ef1ef08
Pedro Ribeiro <pralmeida03@gmail.com>
2025-06-24 16:06:43 -0300

    modularizando as rotas

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,49 +1,24 @@
-import { createBrowserRouter } from "react-router-dom";
-import MainMenu from "./pages/MainMenu";
-import TestSelector from "./pages/TestSelector";
-import ClientMenu from "./pages/ClientMenu";
-import ServerMenu from "./pages/ServerMenu";
-import LogPage from "./pages/LogPage";
-import StatisticsPage from "./pages/StatisticsPage";
-import RoutinePage from "./pages/RoutinePage";
-import AddRoutine from "./pages/AddRoutine";
-import ResultsPage from "./pages/ResultsPage";
-import GetRoutines from "./pages/GetRoutines";
+import { RouteObject } from 'react-router-dom'
+import MainMenu from './pages/MainMenu'
+import ClientMenu from './pages/ClientMenu'
+import ServerMenu from './pages/ServerMenu'
+import TestSelector from './pages/TestSelector'
+import LogPage from './pages/LogPage'
+import StatisticsPage from './pages/StatisticsPage'
+import RoutinePage from './pages/RoutinePage'
+import AddRoutine from './pages/AddRoutine'
+import GetRoutines from './pages/GetRoutines'
+import ResultsPage from './pages/ResultsPage'
 
-
-
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <MainMenu />
-    },{
-        path: "/client",
-        element: <ClientMenu />
-    },{
-        path: "/server",
-        element: <ServerMenu />
-    },{
-        path: "/run",
-        element: <TestSelector />
-    },{
-        path: "/log",
-        element: <LogPage />
-    },{
-        path: "/stats",
-        element: <StatisticsPage />
-    },{
-        path: "/routine",
-        element: <RoutinePage />
-    },{
-        path: "/routine/add",
-        element: <AddRoutine />
-    },{
-        path: "/routine/saved",
-        element: <GetRoutines />
-    },{
-        path: "/results",
-        element: <ResultsPage />
-    }
-])
-
-export default router
+export const routes: RouteObject[] = [
+  { path: '/', element: <MainMenu /> },
+  { path: '/client', element: <ClientMenu /> },
+  { path: '/server', element: <ServerMenu /> },
+  { path: '/run', element: <TestSelector /> },
+  { path: '/log', element: <LogPage /> },
+  { path: '/stats', element: <StatisticsPage /> },
+  { path: '/routine', element: <RoutinePage /> },
+  { path: '/routine/add', element: <AddRoutine /> },
+  { path: '/routine/saved', element: <GetRoutines /> },
+  { path: '/results', element: <ResultsPage /> },
+]
